aa9a87441955a87c3969a43b9bc868ceb9cbda65
Andy Black <blackhandrew@gmail.com>
2026-04-22 09:32:20 -0700

    Linux installer, pass 20

--- installers/linux/createconfigonlyinstaller.sh
+++ installers/linux/createconfigonlyinstaller.sh
@@ -9,6 +9,6 @@ cd ../configuration
 cp XeLaTeXVersion2020.txt XeLaTeXVersion.txt
 cd ../..
 tar -cjf xlingpaperconfig-xxe7.tar XLingPap
-cp xlingpaperconfig-xxe7.tar ../linux
-cd ../linux
+# cp xlingpaperconfig-xxe7.tar ../linux
+# cd ../linux
 tar -czf XLingPaper-$1UpdateConfigOnlySetup.tar.gz installconfigonly-xxe7.sh xlingpaperconfig-xxe7.tar

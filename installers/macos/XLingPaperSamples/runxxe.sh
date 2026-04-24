4d8e77c8beb7d9c8268718f85eeec908422776e6
Andy Black <blackhandrew@gmail.com>
2026-04-24 11:17:27 -0700

    macOS Full installer

---
+++ installers/macos/XLingPaperSamples/runxxe.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# runxxe
+# 
+#
+# Created by Harold Andrew Black on 3/8/10.
+# Copyright 2010-2018 SIL International. All rights reserved.
+/Applications/XMLmind.app/Contents/Resources/xxe/bin/xxe -putprefs "/Users/Shared/XXE8.2/XLingPaper.preferences" "$HOME"/Documents/My_XLingPaper/SamplePaper.xml &
+exit 0
+
---
+++ installers/macos/XXE8.2/copyaddons.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# copyaddons
+# 
+#
+# Created by Harold Andrew Black on 5/11/2018.
+# Copyright 2018 SIL International. All rights reserved.
+
+unzip /Applications/XLingPaper/XXE/es_dictionary-8_2_0.zip -d $HOME/Library/Application\ Support/XMLmind/XMLEditor8/addon
+
+unzip /Applications/XLingPaper/XXE/es_translation-8_2_0.zip -d $HOME/Library/Application\ Support/XMLmind/XMLEditor8/addon
+
+unzip /Applications/XLingPaper/XXE/fr_dictionary-8_2_0.zip -d $HOME/Library/Application\ Support/XMLmind/XMLEditor8/addon
+
+unzip /Applications/XLingPaper/XXE/fr_translation-8_2_0.zip -d $HOME/Library/Application\ Support/XMLmind/XMLEditor8/addon
+
+unzip /Applications/XLingPaper/XXE/sample_customize_xxe-8_2_0.zip -d $HOME/Library/Application\ Support/XMLmind/XMLEditor8/addon
+
+exit 0
+
